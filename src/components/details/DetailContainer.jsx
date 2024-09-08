c1ef5417d264d570eaeaf7ac0810262096c5b990
Iga Tunde <igatunde06@gmail.com>
2024-09-08 23:24:33 +0100

    commit

--- src/components/details/DetailContainer.jsx
+++ src/components/details/DetailContainer.jsx
@@ -8,11 +8,11 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 function DetailContainer() {
   const { filteredMovies } = useContext(MovieContext);
 
-  const firstMovieId = filteredMovies.length > 0 ? filteredMovies[0].id : null;
+  const firstMovieId = filteredMovies.length > 0 ? filteredMovies[4].id : null;
 
   return (
     <div className="lg:pl-56 px-6 pt-5 relative">
-      <button className="border px-5 shadow-xl py-1 rounded-full my-3 dark:border-none dark:bg-darkBlueDarkModeEL dark:shadow-veryDarkBlueLightModeText dark:shadow-md">
+      <button className="border px-5 shadow-xl py-1 rounded-full my-3 dark:border-none dark:bg-darkBlueDarkModeEL dark:shadow-veryDarkBlueLightModeText dark:shadow-md hover:bg-darkBlueDarkModeEL hover:text-white dark:hover:bg-slate-800 transition-transform hover:translate-y-1 ease-in  border-black/30">
         <FontAwesomeIcon icon={faArrowLeft} className="pr-3" />
         Back
       </button>
